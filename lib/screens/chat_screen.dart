75d353939d7af2c51b02aa4fd2dfb84731c1df9b
kartikpachori <kartikeypachori1610@gmail.com>
2024-04-16 23:22:00 +0530

    updates

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     {
       return Scaffold(
-          body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (ctx, index) => Container(
-          padding: EdgeInsets.all(8),
-          child: Text('This works!!'),
+        body: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (ctx, index) => Container(
+            padding: EdgeInsets.all(8),
+            child: Text('This works!!'),
+          ),
         ),
-      ));
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            FirebaseFirestore.instance
+                .collection('chats/8oNReIWzAzxU2nS3jRgG/messages')
+                .snapshots()
+                .listen(
+              (data) {
+                print(data.docs[0].data()['text']);
+              },
+            );
+          },
+        ),
+      );
     }
   }
 }
